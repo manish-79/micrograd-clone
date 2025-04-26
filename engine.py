6dbf711a55342614476ac471eccfce61eba00af7
Manish <103179679+manish-79@users.noreply.github.com>
2025-04-26 10:39:52 +0530

    Added backpropagation and tanh() activation function
    implemented _backward() function for all the mathematical operations like add, multiply etc, so that the gradients are passed backward through all the nodes.
    Also added the tanh() activation function that adds non linearity to the neural network

--- engine.py
+++ engine.py
@@ -1,36 +1,50 @@
-#first lets create a class that can store values and other data related to them
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+%matplotlib inline
 
 class Value:
   def __init__(self,data,label="",_op="",_children=()):
     self.data = data
     self.label = label
+    self.grad = 0.0
     self._op = _op
     self._prev = _children
+    self._backward = lambda : None
 
   def __repr__(self):
     return f"Value(data: {self.data})"
 
   def __add__(self,other):
-    print("started add function")
     other = other if isinstance(other,Value) else Value(other)
-    print(f"add self: {self}, other:{other}")
     out = Value(self.data + other.data, _op="+", _children=(self,other))
+    def _backward():
+      self.grad += 1.0 * out.grad
+      other.grad += 1.0 * out.grad
+    out._backward = _backward
     return out
 
   def __mul__(self,other):
     other = other if isinstance(other,Value) else Value(other)
     out = Value(self.data * other.data, _op="*", _children=(self,other))
+    def _backward():
+      self.grad += other.data * out.grad
+      other.grad += self.data * out.grad
+    out._backward = _backward
     return out
 
   def __pow__(self,other):
     assert isinstance(other,(int,float))
     out = Value(self.data ** other,_op=f"**{other}",_children=(self,))
+    def _backward():
+      self.grad += (other * (self.data **(other-1))) * out.grad
+    out._backward = _backward
     return out
 
   def __truediv__(self,other):
     return self * other**(-1)
 
-  def __rtruediv(self,other):
+  def __rtruediv__(self,other):
     return other * self**(-1)
 
   def __radd__(self,other):
@@ -47,3 +61,27 @@ class Value:
 
   def __rsub__(self,other):
     return other + (-self)
+
+  def tanh(self):
+    x = self.data
+    t = (math.exp(2*x)-1)/(math.exp(2*x)+1)
+    out = Value(t,_op="tanh",_children=(self,))
+    def _backward():
+      self.grad += (1-t**2)*out.grad
+    out._backward = _backward
+    return out
+
+  def backward(self):
+    topo = []
+    visited = set()
+
+    def buildtopo(v):
+        if v not in visited:
+            visited.add(v)
+            for child in v._prev:
+                buildtopo(child)
+            topo.append(v)
+
+
+    for node in reversed(topo):
+      node._backward()
